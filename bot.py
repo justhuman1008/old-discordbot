cc1e95db703ed074e4bdeaee1c120169a719c8e4
그저 평범한 인간 <seanking1008@naver.com>
2021-06-28 17:52:32 +0900

    .
    .

--- bot.py
+++ bot.py
@@ -2,20 +2,30 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix = '-')
+client = discord.Client()
+
+
+token = "NzgwMzY2MDczNjUxMjY1NTM2.X7uCig.LUqtSkbGvKu6mzMhWf-isOIo4Xc"
+
 
 @client.event
 async def on_ready():
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
-
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+    print("=========================")
+    print("아래의 계정으로 로그인 합니다 : ")
+    print(client.user.name)
+    print("연결 성공")
+    game = discord.Game("!도움말")
+    print("=========================")
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
-client.run(os.environ['token'])
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+    if message.content == "!ttest":
+        await message.channel.send("전송 완료")
+        print("메시지 전송 테스트 진행됨")
+
+client.run(token)
