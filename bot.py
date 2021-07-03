54bd8ab8c012ad159d8a745e14e7e3703426995e
그저 평범한 인간 <seanking1008@naver.com>
2021-07-03 13:25:37 +0900

    .
    .

--- bot.py
+++ bot.py
@@ -16,13 +16,10 @@ async def on_ready():
     print("=========================")
     await client.change_presence(status=discord.Status.online, activity=game)
 
-
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return None
-    if message.content == "!ttest":
-        await message.channel.send("전송 완료")
-        print("메시지 전송 테스트 진행됨")
+    if message.content.startswith("!아"): 
+        await message.channel.send("아")
+
 
 client.run(token)
