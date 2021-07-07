78fe2b270ae65488b9e40b97dd414593b26f5763
그저 평범한 인간 <seanking1008@naver.com>
2021-07-07 19:24:56 +0900

    .
    .

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -3,7 +3,8 @@ import discord
 import asyncio
 from discord.ext import commands
 import json
-
+import bs4 #파싱용
+from bs4 import BeautifulSoup #pip install bs4 #실검파싱
 
 class minecraft(commands.Cog):
 
@@ -26,10 +27,19 @@ class minecraft(commands.Cog):
             exceptMojangAPI.add_field(name="­", value=f"Mojang API가 작동하고 있는지 확인해주세요.", inline=False)
             await ctx.send(embed=exceptMojangAPI)
 
+    @commands.command(aliases=['접률']) # 마인리스트 파싱
+    async def uuid(self, ctx,*,message):
+        url = f"https://minelist.kr/servers/{message}"
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
 
-        uuid = "db14a367-08b1-4432-a6d8-4200b69c9083"
-        uuid_dashed = "db14a367-08b1-4432-a6d8-4200b69c9083"
-        self.API_KEY = "ef31f74e-3700-4f99-a572-a96341c19c31"
+        connect = soup.find("div", attrs={"class":"col-md-6 col-sm-6"}).get_text().strip()
+
+        embed = discord.Embed(title="서버접속 현황", description="`"+connect+"`", color=0xffdc16)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/731471072310067221/777089714137595914/externalFile.png")
+        embed.set_footer(text="Information in Minelist")
+        await ctx.send(embed=embed)
+
 
 
 
