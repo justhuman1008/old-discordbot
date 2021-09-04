e8149fc51d1ba44932526512f5a68fcf873ff02f
just <seanking1008@naver.com>
2021-09-04 16:20:11 +0900

    V1.2(2021-09-04) Update

--- cogs/voice.py
+++ cogs/voice.py
@@ -1,26 +1,16 @@
-import discord #pip
+import discord
 from discord.ext import commands
 import asyncio
 #import pyNaCl
-from datetime import datetime # 시간표시용
 
-now = datetime.now()
+# 시간대 설정
 
 class voice(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['음성'])
-    async def _voicehelp(self, ctx):
-        embed = discord.Embed(title="음성 채널 관련 명령어", description="­", color=0xffdc16)
-        embed.add_field(name=':small_blue_diamond:'+"!참가", value="유저가 참여중인 음성채널에 연결합니다.", inline=False)
-        embed.add_field(name=':small_blue_diamond:'+"!나가", value="음성채널에서 나갑니다.", inline=False)
-        embed.add_field(name=':small_blue_diamond:'+"!음소거", value="봇의 마이크를 끕니다.", inline=False)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/731471072310067221/870547423196499968/3faa9d34cc341657.png')
-        await ctx.send(embed = embed)
-
-    @commands.command(aliases=['참가', '들어와', '참여'])
+    @commands.command(aliases=['참가', '들어와', '참여']) # Com2
     async def _join(self, ctx):
         if ctx.author.voice and ctx.author.voice.channel: # 채널에 들어가 있는지 파악
             channel = ctx.author.voice.channel # 채널 구하기
@@ -29,10 +19,10 @@ class voice(commands.Cog):
             else:
                 await channel.connect() # 채널 연결
                 await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
-        else: # 유저가 채널에 없으면
+        else: # 유저가 채널에 없으면 
             await ctx.send(embed=discord.Embed(title=f'음성채널에 유저가 없습니다.', description='음성 채널에 참가해주세요', color=0xf8e71c)) # 출력
 
-    @commands.command(aliases=['나가'])
+    @commands.command(aliases=['나가']) # Com3
     async def _stop(self, ctx):
         if ctx.author.voice and ctx.author.voice.channel:
             channel = ctx.author.voice.channel # 채널 구하기
@@ -43,7 +33,7 @@ class voice(commands.Cog):
         else:
             await ctx.send(embed=discord.Embed(title=f'당신은 음성채널에 연결되어 있지 않습니다.', description='음성채널에 참여한 유저만 `!나가`를 사용할 수 있습니다.', color=0xf8e71c)) # 출력
 
-    @commands.command(aliases=['음소거'])
+    @commands.command(aliases=['음소거']) # Com4
     async def _mute(self, ctx):
         if ctx.author.voice and ctx.author.voice.channel:
             channel = ctx.author.voice.channel # 채널 구하기
