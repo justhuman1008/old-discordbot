f90f6e71b32d976d80a2893d2d6f8361c2d85b03
just <seanking1008@naver.com>
2021-11-16 12:22:54 +0900

    V1.2.2(2021-11-16) Update

--- cogs/hy.py
+++ cogs/hy.py
@@ -1,5 +1,6 @@
 import discord #pip
 from discord.ext import commands, tasks
+import asyncio
 
 
 
@@ -15,8 +16,6 @@ class hy(commands.Cog):
     async def _hyhelp(self, ctx):
         embed = discord.Embed(title="해당 명령어는 준비중입니다.", description="`준비중`", color=0xffdc16)
         await ctx.send(embed = embed)
-
-
 
 
 
